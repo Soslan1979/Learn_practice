2e7d412d5aa89a42f4c94c76f9a724a6d51e2d39
geekbrains <info@geekbrains.ru>
2022-02-15 20:51:25 +0300

    1111

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -2,7 +2,7 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        Team person = new Team("Max11", 5, 6, 3);
+        Team person = new Team("Max1111", 5, 6, 3);
         System.out.println(person.run(3500));
     }
 
